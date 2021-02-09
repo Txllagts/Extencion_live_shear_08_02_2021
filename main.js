df9ec01f1f2141989daaff392c16ff1499bff633
Jasbeith Andrea Prada Hernández <jasbeith2505@gmail.com>
2021-02-08 21:22:02 -0500

    Extencion_live_shear_08_02_2021

---
+++ main.js
@@ -0,0 +1,18 @@
+let cadena = "Hola a todos BIENVENIDOS al curso de Javascript";
+
+//let resultado = cadena.startsWith("Hola");
+//let resultado = cadena.endsWith("Javascript");
+//let resultado = cadena.includes("curso");
+//let resultado = cadena.indexOf("e");
+//let resultado = cadena.LastIndexOf("e");
+
+//let resultado = cadena.repeat(5);
+
+//let resultado = cadena.split(" ", 6);
+//let resultado = cadena.substring(0, 12);
+//let resultado = cadena.toUpperCase();
+//let resultado = cadena.toLowerCase();
+
+
+
+console.log(resultado)
